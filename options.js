023050e9f929d0db420b283da4396e40a0cf213b
jermowery <jermowery@gmail.com>
2021-05-22 16:23:26 -0700

    Make options better

--- options.js
+++ options.js
@@ -1,4 +1,5 @@
 import { getOptionsWithDefaults, saveOptionPageOptions } from './option-saver.js';
+import { setInputValue } from './utils.js';
 
 /** @type {HTMLInputElement} */
 const defaultBody = document.getElementById("default_body");
@@ -6,10 +7,13 @@ const saveButton = document.getElementById("save");
 
 // Import default body text from storage.
 getOptionsWithDefaults((options) => {
-    defaultBody.value = options['default_body'];
+    setInputValue(defaultBody, options['default_body']);
 });
 
 // When the button is clicked, save the default body text (without overriding other options).
 saveButton.addEventListener("click", async () => {
     saveOptionPageOptions({ 'default_body': defaultBody.value });
 });
+
+// Set focus for a11y.
+defaultBody.focus();
