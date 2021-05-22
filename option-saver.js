aa46b60cc7431f482d961c4b3521b7128d9afe06
irrationalpie7 <irrationalpie7@gmail.com>
2021-05-22 13:43:05 -0700

    Tweak option/popup user text.

--- option-saver.js
+++ option-saver.js
@@ -9,11 +9,6 @@ const default_options = {
     'default_body': 'Here are a few building blocks that that show how you can include an image, audio, or a link to your podfic in your post. They\'re all optional, and you can change these defaults to match your own default posting template by going to the option page for this extension. Happy posting!\n\n<img src="IMAGE_URL" width="500px" alt="Cover art. COVER_DESCRIPTION." />\n\n<audio src="PODFIC_URL_ENDING_IN_MP3" controls="controls" crossorigin="anonymous" preload="metadata"/>\n\n<a href="PODFIC_URL" rel="nofollow">Download the podfic here (FILE_SIZE MB/FILE_MINUTES minutes)</a>.'
 };
 
-export function resetOptions() {
-    options = default_options;
-    chrome.storage.sync.set({ options });
-}
-
 /**
  * Fetches options, setting them all to defaults if it can't find them.
  * @param callBack 
@@ -37,7 +32,7 @@ export function savePopUpOptions(popUpOptions) {
         if (typeof options === 'undefined') {
             options = default_options;
         }
-        // Override the new default_body value.
+        // Use saved default_body value rather than whatever is in popUpOptions.
         popUpOptions['default_body'] = options['default_body'];
         options = popUpOptions;
         // Save.
