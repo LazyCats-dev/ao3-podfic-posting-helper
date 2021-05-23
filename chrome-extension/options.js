5869f3cabdf001e93bf94d7d40b098cfd15b108e
jermowery <jermowery@gmail.com>
2021-05-22 23:15:13 -0700

    Shown snack bar when options finish saving

--- chrome-extension/options.js
+++ chrome-extension/options.js
@@ -5,6 +5,7 @@ import { setInputValue } from './utils.js';
 const defaultBody = document.getElementById("default_body");
 /** @type {HTMLFormElement} */
 const form = document.getElementById("form");
+const snackbar = document.querySelector(".mdc-snackbar").MDCSnackbar;
 
 // Import default body text from storage.
 getOptionsWithDefaults((options) => {
@@ -15,6 +16,7 @@ getOptionsWithDefaults((options) => {
 form.addEventListener("submit", async submitEvent => {
     submitEvent.preventDefault();
     saveOptionPageOptions({ 'default_body': defaultBody.value });
+    snackbar.open();
 });
 
 // Set focus for a11y.
