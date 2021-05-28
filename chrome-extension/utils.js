990d06bb1ceffd62ed779e1384b93e6f18a4ffe2
jermowery <jermowery@gmail.com>
2021-05-28 10:37:33 -0700

    clang-format the rest of the JS

--- chrome-extension/utils.js
+++ chrome-extension/utils.js
@@ -1,27 +1,24 @@
 /**
  * Sets the value of the input, triggering all necessary events.
- * @param inputElement {HTMLInputElement} 
+ * @param inputElement {HTMLInputElement}
  * @param value {string}
  */
- export function setInputValue(inputElement, value) {
-    const event = new InputEvent('input', {
-        bubbles: true,
-        data: value
-    });
-    inputElement.value = value;
-    // Replicates the value changing.
-    inputElement.dispatchEvent(event);
-    // Replicates the user leaving focus of the input element.
-    inputElement.dispatchEvent(new Event('change'));
+export function setInputValue(inputElement, value) {
+  const event = new InputEvent('input', {bubbles: true, data: value});
+  inputElement.value = value;
+  // Replicates the value changing.
+  inputElement.dispatchEvent(event);
+  // Replicates the user leaving focus of the input element.
+  inputElement.dispatchEvent(new Event('change'));
 }
 
 /**
  * Sets the state of a checkbox, triggering all necessary events.
- * @param checkboxElement {HTMLInputElement} 
+ * @param checkboxElement {HTMLInputElement}
  * @param isChecked {boolean}
  */
 export function setCheckboxState(checkboxElement, isChecked) {
-    checkboxElement.checked = isChecked;
-    // Replicates the user leaving focus of the input element.
-    checkboxElement.dispatchEvent(new Event('change'));
+  checkboxElement.checked = isChecked;
+  // Replicates the user leaving focus of the input element.
+  checkboxElement.dispatchEvent(new Event('change'));
 }
