c6c5f434ab08e315b614ec8bb6cc5f9e57a6885c
jermowery <jermowery@gmail.com>
2021-05-31 15:27:43 -0700

    Really finish the migration

--- chrome-extension/options.js
+++ chrome-extension/options.js
@@ -7,15 +7,15 @@ const form = document.getElementById('form');
 const snackbar = document.querySelector('.mdc-snackbar').MDCSnackbar;
 
 // Import default body text from storage.
-chrome.storage.sync.get('workbody', async ({workbody}) => {
-  setInputValue(defaultBody, workbody['default']);
-});
+
+const {workbody} = await browser.storage.sync.get('workbody');
+setInputValue(defaultBody, workbody['default']);
 
 // When the form is submitted, save the default body text (without overriding
 // other options).
 form.addEventListener('submit', async submitEvent => {
   submitEvent.preventDefault();
-  chrome.storage.sync.set({'workbody': {'default': defaultBody.value}});
+  await chrome.storage.sync.set({'workbody': {'default': defaultBody.value}});
   snackbar.open();
 });
 
