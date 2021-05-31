38e352bff8a7533de7dd2af56b1d1bd1c5830cb8
jermowery <jermowery@gmail.com>
2021-05-30 21:24:17 -0700

    [broken]: changes to make the code compatible with firefox

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -1,20 +1,19 @@
 import {setCheckboxState, setInputValue} from './utils.js';
 
-
 /** @type {HTMLButtonElement} */
 const optionsButton = document.getElementById('options_button');
 
 optionsButton.addEventListener('click', () => {
-  if (chrome.runtime.openOptionsPage) {
-    chrome.runtime.openOptionsPage();
+  if (browser.runtime.openOptionsPage) {
+    browser.runtime.openOptionsPage();
   } else {
-    window.open(chrome.runtime.getURL('options.html'));
+    window.open(browser.runtime.getURL('options.html'));
   }
 });
 
 (async () => {
   const [currentTab] =
-      await chrome.tabs.query({active: true, currentWindow: true});
+      await browser.tabs.query({active: true, currentWindow: true});
   if (currentTab.url !== 'https://archiveofourown.org/works/new') {
     document.querySelector('.page-content').innerHTML =
         `To use this extension go to
@@ -25,11 +24,11 @@ optionsButton.addEventListener('click', () => {
                 https://archiveofourown.org/works/new</a>
         and then click on the extension icon again`;
   } else {
-    setupPopup();
+    await setupPopup();
   }
 })();
 
-function setupPopup() {
+async function setupPopup() {
   /** @type {HTMLInputElement} */
   const urlInput = document.getElementById('url-input');
   /** @type {HTMLFormElement} */
@@ -50,28 +49,28 @@ function setupPopup() {
 
   // Import pop-up options from storage.
 
-  chrome.storage.sync.get('options', ({options}) => {
-    setInputValue(urlInput, options['url']);
-    setCheckboxState(podficLabel, options['podfic_label']);
-    setCheckboxState(podficLengthLabel, options['podfic_length_label']);
-    setCheckboxState(transformSummary, options['transform_summary']);
-    setCheckboxState(transformTitle, options['transform_title']);
+  const {options} = await browser.storage.sync.get('options');
 
-    // Podfic length value has special considerations
-    const selectElement = document.getElementById('podfic-length-select');
-    const selectInputElement = selectElement.querySelector('input');
-    setInputValue(selectInputElement, options['podfic_length_value']);
-    // For some reason a select is really stupid so we have to find the option
-    // with the correct text and click it.
-    const optionElements = selectElement.querySelectorAll('.mdc-list-item');
-    const optionMatchingValue = Array.from(optionElements)
-                                    .find(
-                                        option => option.dataset.value ===
-                                            options['podfic_length_value']);
-    if (optionMatchingValue) {
-      optionMatchingValue.click();
-    }
-  });
+  setInputValue(urlInput, options['url']);
+  setCheckboxState(podficLabel, options['podfic_label']);
+  setCheckboxState(podficLengthLabel, options['podfic_length_label']);
+  setCheckboxState(transformSummary, options['transform_summary']);
+  setCheckboxState(transformTitle, options['transform_title']);
+
+  // Podfic length value has special considerations
+  const selectElement = document.getElementById('podfic-length-select');
+  const selectInputElement = selectElement.querySelector('input');
+  setInputValue(selectInputElement, options['podfic_length_value']);
+  // For some reason a select is really stupid so we have to find the option
+  // with the correct text and click it.
+  const optionElements = selectElement.querySelectorAll('.mdc-list-item');
+  const optionMatchingValue = Array.from(optionElements)
+                                  .find(
+                                      option => option.dataset.value ===
+                                          options['podfic_length_value']);
+  if (optionMatchingValue) {
+    optionMatchingValue.click();
+  }
 
   // When the form is submitted, import metadata from original work.
   form.addEventListener('submit', async (submitEvent) => {
@@ -80,7 +79,7 @@ function setupPopup() {
     urlTextField.helperTextContent = '';
     // Save the options, because we won't be able to access them later.
 
-    chrome.storage.sync.set({
+    await browser.storage.sync.set({
       'options': {
         'url': urlInput.value,
         'podfic_label': podficLabel.checked,
@@ -306,8 +305,8 @@ function setupPopup() {
      * Fill in the new work page with the extracted metadata.
      */
     async function fillMetadata() {
-      function fillMetadataInjected() {
-        (() => {
+      async function fillMetadataInjected() {
+        await (async () => {
           // Duplicated because they need to exist in the injected script.
           /**
            * Query a (potentially empty) list of HTMLElements
@@ -413,128 +412,117 @@ function setupPopup() {
             inputElement.dispatchEvent(
                 new KeyboardEvent('keydown', {'key': ','}));
           }
+          const {options, metadata, workbody} = await browser.storage.sync.get(
+              ['metadata', 'options', 'workbody']);
+          const newWorkPage = document.getElementById('main');
 
-          chrome.storage.sync.get(
-              ['metadata', 'options', 'workbody'], ({
-                                                     options,
-                                                     metadata,
-                                                     workbody,
-                                                   }) => {
-                const newWorkPage = document.getElementById('main');
+          // Find the rating drop down, and pick the correct value.
+          const ratingSelect = queryElement(newWorkPage, '#work_rating_string');
+          const ratingOptions = mapOptions(ratingSelect);
+          ratingSelect.value = ratingOptions.get(metadata['rating']);
 
-                // Find the rating drop down, and pick the correct value.
-                const ratingSelect =
-                    queryElement(newWorkPage, '#work_rating_string');
-                const ratingOptions = mapOptions(ratingSelect);
-                ratingSelect.value = ratingOptions.get(metadata['rating']);
+          // Find the warning check boxes, and check all the ones that
+          // apply.
+          const warningBoxes = mapInputs(queryElements(
+              queryElement(newWorkPage, 'fieldset.warnings'), 'input'));
+          warningBoxes.set(
+              'Creator Chose Not To Use Archive Warnings',
+              warningBoxes.get('Choose Not To Use Archive Warnings'));
+          metadata['warnings'].forEach(
+              warning => warningBoxes.get(warning).checked = true);
 
-                // Find the warning check boxes, and check all the ones that
-                // apply.
-                const warningBoxes = mapInputs(queryElements(
-                    queryElement(newWorkPage, 'fieldset.warnings'), 'input'));
-                warningBoxes.set(
-                    'Creator Chose Not To Use Archive Warnings',
-                    warningBoxes.get('Choose Not To Use Archive Warnings'));
-                metadata['warnings'].forEach(
-                    warning => warningBoxes.get(warning).checked = true);
+          // Find the fandom text input, and insert a comma-separated list
+          // of fandoms. Tell ao3 we did so.
+          const fandomInput =
+              queryElement(queryElement(newWorkPage, 'dd.fandom'), 'input');
+          setTagsInputValue(fandomInput, metadata['fandoms'].join(', '));
 
-                // Find the fandom text input, and insert a comma-separated list
-                // of fandoms. Tell ao3 we did so.
-                const fandomInput = queryElement(
-                    queryElement(newWorkPage, 'dd.fandom'), 'input');
-                setTagsInputValue(fandomInput, metadata['fandoms'].join(', '));
+          // Find the category check boxes, and check all the ones that
+          // apply.
+          const categoryBoxes = mapInputs(
+              queryElements(queryElement(newWorkPage, 'dd.category'), 'input'));
+          metadata['categories'].forEach(
+              category => categoryBoxes.get(category).checked = true);
 
-                // Find the category check boxes, and check all the ones that
-                // apply.
-                const categoryBoxes = mapInputs(queryElements(
-                    queryElement(newWorkPage, 'dd.category'), 'input'));
-                metadata['categories'].forEach(
-                    category => categoryBoxes.get(category).checked = true);
+          // Find the relationship text input, and insert a
+          // comma-separated list of relationships. Tell ao3 we did so.
+          const relationshipInput = queryElement(
+              queryElement(newWorkPage, 'dd.relationship'), 'input');
+          setTagsInputValue(
+              relationshipInput, metadata['relationships'].join(', '));
 
-                // Find the relationship text input, and insert a
-                // comma-separated list of relationships. Tell ao3 we did so.
-                const relationshipInput = queryElement(
-                    queryElement(newWorkPage, 'dd.relationship'), 'input');
-                setTagsInputValue(
-                    relationshipInput, metadata['relationships'].join(', '));
+          // Find the character input, and insert a comma-separated list
+          // of characters. Tell ao3 we did so.
+          const characterInput =
+              queryElement(queryElement(newWorkPage, 'dd.character'), 'input');
+          setTagsInputValue(characterInput, metadata['characters'].join(', '));
 
-                // Find the character input, and insert a comma-separated list
-                // of characters. Tell ao3 we did so.
-                const characterInput = queryElement(
-                    queryElement(newWorkPage, 'dd.character'), 'input');
-                setTagsInputValue(
-                    characterInput, metadata['characters'].join(', '));
+          // Find the freeform tags input, and insert a comma-separated
+          // list of freeform tags. (potentially auto-adding "Podfic" and
+          // "Podfic Length" tags) Tell ao3 we did so.
+          const additionalTagsInput =
+              queryElement(queryElement(newWorkPage, 'dd.freeform'), 'input');
+          if (options['podfic_label']) {
+            metadata['freeformTags'].push('Podfic');
+          }
+          if (options['podfic_length_label']) {
+            metadata['freeformTags'].push(
+                'Podfic Length: ' + options['podfic_length_value']);
+          }
+          setTagsInputValue(
+              additionalTagsInput, metadata['freeformTags'].join(', '));
 
-                // Find the freeform tags input, and insert a comma-separated
-                // list of freeform tags. (potentially auto-adding "Podfic" and
-                // "Podfic Length" tags) Tell ao3 we did so.
-                const additionalTagsInput = queryElement(
-                    queryElement(newWorkPage, 'dd.freeform'), 'input');
-                if (options['podfic_label']) {
-                  metadata['freeformTags'].push('Podfic');
-                }
-                if (options['podfic_length_label']) {
-                  metadata['freeformTags'].push(
-                      'Podfic Length: ' + options['podfic_length_value']);
-                }
-                setTagsInputValue(
-                    additionalTagsInput, metadata['freeformTags'].join(', '));
+          // Set the title.
+          const titleInput =
+              queryElement(queryElement(newWorkPage, 'dd.title'), 'input');
+          if (options['transform_title']) {
+            titleInput.value = '[Podfic] ' + metadata['title'];
+          } else {
+            titleInput.value = metadata['title'];
+          }
 
-                // Set the title.
-                const titleInput = queryElement(
-                    queryElement(newWorkPage, 'dd.title'), 'input');
-                if (options['transform_title']) {
-                  titleInput.value = '[Podfic] ' + metadata['title'];
-                } else {
-                  titleInput.value = metadata['title'];
-                }
+          // Set the summary, optionally wrapping it in a block quote.
+          const summaryTextArea =
+              queryElement(queryElement(newWorkPage, 'dd.summary'), 'textarea');
+          if (options['transform_summary']) {
+            summaryTextArea.value = transform(
+                metadata['summary'], metadata['title'], metadata['url'],
+                new Map(metadata['authors']));
+          } else {
+            summaryTextArea.value = metadata['summary'];
+          }
 
-                // Set the summary, optionally wrapping it in a block quote.
-                const summaryTextArea = queryElement(
-                    queryElement(newWorkPage, 'dd.summary'), 'textarea');
-                if (options['transform_summary']) {
-                  summaryTextArea.value = transform(
-                      metadata['summary'], metadata['title'], metadata['url'],
-                      new Map(metadata['authors']));
-                } else {
-                  summaryTextArea.value = metadata['summary'];
-                }
+          // Set the "inspired by" work url.
+          const parentCheckmark =
+              queryElement(queryElement(newWorkPage, 'dt.parent'), 'input');
+          if (!parentCheckmark.checked) {
+            parentCheckmark.click();
+          }
+          const parentUrl =
+              queryElement(newWorkPage, '#work_parent_attributes_url');
+          parentUrl.value = metadata['url'];
 
-                // Set the "inspired by" work url.
-                const parentCheckmark = queryElement(
-                    queryElement(newWorkPage, 'dt.parent'), 'input');
-                if (!parentCheckmark.checked) {
-                  parentCheckmark.click();
-                }
-                const parentUrl =
-                    queryElement(newWorkPage, '#work_parent_attributes_url');
-                parentUrl.value = metadata['url'];
+          // Set the same language as the original work.
+          const languageSelect = queryElement(newWorkPage, '#work_language_id');
+          const languageOptions = mapOptions(languageSelect);
+          languageSelect.value = languageOptions.get(metadata['language']);
 
-                // Set the same language as the original work.
-                const languageSelect =
-                    queryElement(newWorkPage, '#work_language_id');
-                const languageOptions = mapOptions(languageSelect);
-                languageSelect.value =
-                    languageOptions.get(metadata['language']);
-
-                // Set the new work text.
-                const workText = queryElement(newWorkPage, '.mce-editor');
-                // If there's nothing here yet, over-write it.
-                if (workText.value == '') {
-                  workText.value = workbody['default'];
-                }
-              });
+          // Set the new work text.
+          const workText = queryElement(newWorkPage, '.mce-editor');
+          // If there's nothing here yet, over-write it.
+          if (workText.value == '') {
+            workText.value = workbody['default'];
+          }
         })();
       }
 
       const [tab] =
-          await chrome.tabs.query({active: true, currentWindow: true});
+          await browser.tabs.query({active: true, currentWindow: true});
 
-      chrome.scripting.executeScript({
-        target: {tabId: tab.id},
-        // main just waits for importAndFillMetadata
-        function: fillMetadataInjected,
-      });
+      await browser.tabs.executeScript(
+          tab.id,
+          {code: "fillMetadataInjected()"},
+      );
     }
 
     // The body of this function will be executed as a content script inside the
@@ -542,7 +530,7 @@ function setupPopup() {
     async function importAndFillMetadata() {
       const metadata = await importMetadata(urlInput.value);
       if (metadata) {
-        chrome.storage.sync.set({metadata});
+        await browser.storage.sync.set({metadata});
         await fillMetadata();
         snackbar.open();
       }
