471ccdd7b32682470c7cb7c3d61e42d6210f5410
Jeremy Mowery <jermowery@gmail.com>
2023-05-01 16:47:43 -0700

    Format JS

--- src/options.js
+++ src/options.js
@@ -1,168 +1,164 @@
-import { setCheckboxState, setInputValue, setupStorage } from "./utils.js";
+import {setCheckboxState, setInputValue, setupStorage} from './utils.js';
 
 (async () => {
   await setupStorage();
 
   // Sets up the HTMlSanitizer with the tags that ao3 allows.
-  [
-    "a",
-    "abbr",
-    "acronym",
-    "address",
-    "audio",
-    "b",
-    "big",
-    "blockquote",
-    "br",
-    "caption",
-    "center",
-    "cite",
-    "code",
-    "col",
-    "colgroup",
-    "dd",
-    "del",
-    "details",
-    "dfn",
-    "div",
-    "dl",
-    "dt",
-    "em",
-    "h1",
-    "h2",
-    "h3",
-    "h4",
-    "h5",
-    "h6",
-    "hr",
-    "i",
-    "iframe",
-    "img",
-    "ins",
-    "kbd",
-    "li",
-    "ol",
-    "p",
-    "pre",
-    "q",
-    "s",
-    "samp",
-    "small",
-    "span",
-    "strike",
-    "strong",
-    "sub",
-    "summary",
-    "sup",
-    "table",
-    "tbody",
-    "td",
-    "tfoot",
-    "th",
-    "thead",
-    "tr",
-    "tt",
-    "u",
-    "ul",
-    "var",
+  ['a',
+   'abbr',
+   'acronym',
+   'address',
+   'audio',
+   'b',
+   'big',
+   'blockquote',
+   'br',
+   'caption',
+   'center',
+   'cite',
+   'code',
+   'col',
+   'colgroup',
+   'dd',
+   'del',
+   'details',
+   'dfn',
+   'div',
+   'dl',
+   'dt',
+   'em',
+   'h1',
+   'h2',
+   'h3',
+   'h4',
+   'h5',
+   'h6',
+   'hr',
+   'i',
+   'iframe',
+   'img',
+   'ins',
+   'kbd',
+   'li',
+   'ol',
+   'p',
+   'pre',
+   'q',
+   's',
+   'samp',
+   'small',
+   'span',
+   'strike',
+   'strong',
+   'sub',
+   'summary',
+   'sup',
+   'table',
+   'tbody',
+   'td',
+   'tfoot',
+   'th',
+   'thead',
+   'tr',
+   'tt',
+   'u',
+   'ul',
+   'var',
   ].forEach((tag) => {
-    HtmlSanitizer.AllowedTags[tag.toLocaleUpperCase("en-US")] = true;
+    HtmlSanitizer.AllowedTags[tag.toLocaleUpperCase('en-US')] = true;
   });
 
   // AO3 does not allow styles
-  HtmlSanitizer.AllowedAttributes["style"] = false;
-  HtmlSanitizer.AllowedAttributes["rel"] = true;
-  HtmlSanitizer.AllowedAttributes["alt"] = true;
-  HtmlSanitizer.AllowedAttributes["crossorigin"] = true;
-  HtmlSanitizer.AllowedAttributes["preload"] = true;
+  HtmlSanitizer.AllowedAttributes['style'] = false;
+  HtmlSanitizer.AllowedAttributes['rel'] = true;
+  HtmlSanitizer.AllowedAttributes['alt'] = true;
+  HtmlSanitizer.AllowedAttributes['crossorigin'] = true;
+  HtmlSanitizer.AllowedAttributes['preload'] = true;
 
   const DOM_PARSER = new DOMParser();
 
   hljs.highlightAll();
 
   /** @type {HTMLInputElement} */
-  const titleTemplate = document.getElementById("title_template");
+  const titleTemplate = document.getElementById('title_template');
   /** @type {HTMLFormElement} */
-  const titleForm = document.getElementById("title_form");
+  const titleForm = document.getElementById('title_form');
   /** @type {HTMLInputElement} */
-  const titlePreview = document.getElementById("title_preview");
+  const titlePreview = document.getElementById('title_preview');
   /** @type {MDCTextField} */
-  const titleTextField = titleTemplate.closest(".mdc-text-field").MDCTextField;
+  const titleTextField = titleTemplate.closest('.mdc-text-field').MDCTextField;
   /** @type {HTMLInputElement} */
-  const summaryTemplate = document.getElementById("summary_template");
+  const summaryTemplate = document.getElementById('summary_template');
   const summaryTemplateTextField =
-    summaryTemplate.closest(".mdc-text-field").MDCTextField;
+      summaryTemplate.closest('.mdc-text-field').MDCTextField;
   /** @type {HTMLElement} */
-  const summaryPreview = document.getElementById("summary_preview");
+  const summaryPreview = document.getElementById('summary_preview');
   /** @type {HTMLFormElement} */
-  const summaryForm = document.getElementById("summary_form");
-  const notesTemplate = document.getElementById("notes_template");
+  const summaryForm = document.getElementById('summary_form');
+  const notesTemplate = document.getElementById('notes_template');
   const notesTemplateTextField =
-    notesTemplate.closest(".mdc-text-field").MDCTextField;
+      notesTemplate.closest('.mdc-text-field').MDCTextField;
   /** @type {HTMLElement} */
-  const notesPreview = document.getElementById("notes_preview");
+  const notesPreview = document.getElementById('notes_preview');
   /** @type {HTMLFormElement} */
-  const notesForm = document.getElementById("notes_form");
+  const notesForm = document.getElementById('notes_form');
   /** @type {HTMLInputElement} */
-  const defaultBody = document.getElementById("default_body");
+  const defaultBody = document.getElementById('default_body');
   const defaultBodyTextField =
-    defaultBody.closest(".mdc-text-field").MDCTextField;
+      defaultBody.closest('.mdc-text-field').MDCTextField;
   /** @type {HTMLElement} */
-  const defaultBodyPreview = document.getElementById("default_body_preview");
+  const defaultBodyPreview = document.getElementById('default_body_preview');
   /** @type {HTMLFormElement} */
-  const workForm = document.getElementById("work_form");
-  const snackbar = document.querySelector(".mdc-snackbar").MDCSnackbar;
+  const workForm = document.getElementById('work_form');
+  const snackbar = document.querySelector('.mdc-snackbar').MDCSnackbar;
   /** @type {HTMLButtonElement} */
-  const titleResetButton = document.getElementById("title_reset");
+  const titleResetButton = document.getElementById('title_reset');
   /** @type {HTMLButtonElement} */
-  const summaryResetButton = document.getElementById("summary_reset");
+  const summaryResetButton = document.getElementById('summary_reset');
   /** @type {HTMLButtonElement} */
-  const notesResetButton = document.getElementById("notes_reset");
+  const notesResetButton = document.getElementById('notes_reset');
   /** @type {HTMLInputElement} */
-  const beginningNotesCheckbox = document.getElementById("beginning_notes");
+  const beginningNotesCheckbox = document.getElementById('beginning_notes');
   /** @type {HTMLInputElement} */
-  const endNotesCheckbox = document.getElementById("end_notes");
+  const endNotesCheckbox = document.getElementById('end_notes');
 
-  titleResetButton.addEventListener("click", async () => {
-    const { title_template } = await browser.storage.sync.get("title_template");
-    setInputValue(titleTemplate, "[Podfic] ${title}");
+  titleResetButton.addEventListener('click', async () => {
+    const {title_template} = await browser.storage.sync.get('title_template');
+    setInputValue(titleTemplate, '[Podfic] ${title}');
   });
 
-  summaryResetButton.addEventListener("click", async () => {
-    const { summary_template } = await browser.storage.sync.get(
-      "summary_template"
-    );
+  summaryResetButton.addEventListener('click', async () => {
+    const {summary_template} =
+        await browser.storage.sync.get('summary_template');
     setInputValue(
-      summaryTemplate,
-      "${blocksummary}Podfic of ${title} by ${authors}."
-    );
+        summaryTemplate, '${blocksummary}Podfic of ${title} by ${authors}.');
   });
 
-  notesResetButton.addEventListener("click", async () => {
-    const { notes_template } = await browser.storage.sync.get("notes_template");
+  notesResetButton.addEventListener('click', async () => {
+    const {notes_template} = await browser.storage.sync.get('notes_template');
 
-    setInputValue(notesTemplate, "");
+    setInputValue(notesTemplate, '');
     setCheckboxState(beginningNotesCheckbox, false);
     setCheckboxState(endNotesCheckbox, false);
   });
 
   titleTextField.useNativeValidation = false;
 
-  titleTemplate.addEventListener("input", (event) => {
-    titlePreview.textContent = event.target.value
-      .replaceAll("${title}", "TITLE_TEXT")
-      .replaceAll("${title-unlinked}", "TITLE_TEXT")
-      .replaceAll("${authors}", "AUTHOR_1, AUTHOR_2")
-      .replaceAll("${author}", "AUTHOR_1, AUTHOR_2")
-      .replaceAll("${authors-unlinked}", "AUTHOR_1, AUTHOR_2")
-      .replaceAll("${author-unlinked}", "AUTHOR_1, AUTHOR_2");
+  titleTemplate.addEventListener('input', (event) => {
+    titlePreview.textContent =
+        event.target.value.replaceAll('${title}', 'TITLE_TEXT')
+            .replaceAll('${title-unlinked}', 'TITLE_TEXT')
+            .replaceAll('${authors}', 'AUTHOR_1, AUTHOR_2')
+            .replaceAll('${author}', 'AUTHOR_1, AUTHOR_2')
+            .replaceAll('${authors-unlinked}', 'AUTHOR_1, AUTHOR_2')
+            .replaceAll('${author-unlinked}', 'AUTHOR_1, AUTHOR_2');
     hljs.highlightElement(titlePreview);
     if (isHtml(event.target.value)) {
       titleTextField.helperTextContent =
-        "This template should not contain HTML but it appears to contain HTML";
+          'This template should not contain HTML but it appears to contain HTML';
       titleTextField.valid = false;
     } else {
-      titleTextField.helperTextContent = "";
+      titleTextField.helperTextContent = '';
       titleTextField.valid = true;
     }
   });
@@ -172,56 +168,44 @@ import { setCheckboxState, setInputValue, setupStorage } from "./utils.js";
   }
 
   attachHTMLPreviewAndValidateListeners(
-    summaryTemplateTextField,
-    summaryTemplate,
-    summaryPreview
-  );
+      summaryTemplateTextField, summaryTemplate, summaryPreview);
 
   attachHTMLPreviewAndValidateListeners(
-    notesTemplateTextField,
-    notesTemplate,
-    notesPreview
-  );
+      notesTemplateTextField, notesTemplate, notesPreview);
 
   attachHTMLPreviewAndValidateListeners(
-    defaultBodyTextField,
-    defaultBody,
-    defaultBodyPreview
-  );
+      defaultBodyTextField, defaultBody, defaultBodyPreview);
 
   function attachHTMLPreviewAndValidateListeners(
-    /** @type{MDCTextField}*/ templateTextField,
-    /** @type{HTMLInputElement}*/ template,
-    /** @type{HTMLElement}*/ preview
-  ) {
+      /** @type{MDCTextField}*/ templateTextField,
+      /** @type{HTMLInputElement}*/ template,
+      /** @type{HTMLElement}*/ preview) {
     templateTextField.useNativeValidation = false;
 
-    template.addEventListener("input", (event) => {
+    template.addEventListener('input', (event) => {
       const previewHtml = HtmlSanitizer.SanitizeHtml(
-        event.target.value
-          .replaceAll(
-            "${blocksummary}",
-            "<blockquote>BLOCK_SUMMARY_TEXT</blockquote>"
-          )
-          .replaceAll("${summary}", "SUMMARY_TEXT")
-          .replaceAll("${title}", "<a>TITLE_TEXT</a>")
-          .replaceAll("${title-unlinked}", "TITLE_TEXT")
-          .replaceAll("${authors}", "<a>AUTHOR_1</a>, <a>AUTHOR_2</a>")
-          .replaceAll("${author}", "<a>AUTHOR_1</a>, <a>AUTHOR_2</a>")
-          .replaceAll("${authors-unlinked}", "AUTHOR_1, AUTHOR_2")
-          .replaceAll("${author-unlinked}", "AUTHOR_1, AUTHOR_2")
-      );
+          event.target.value
+              .replaceAll(
+                  '${blocksummary}',
+                  '<blockquote>BLOCK_SUMMARY_TEXT</blockquote>')
+              .replaceAll('${summary}', 'SUMMARY_TEXT')
+              .replaceAll('${title}', '<a>TITLE_TEXT</a>')
+              .replaceAll('${title-unlinked}', 'TITLE_TEXT')
+              .replaceAll('${authors}', '<a>AUTHOR_1</a>, <a>AUTHOR_2</a>')
+              .replaceAll('${author}', '<a>AUTHOR_1</a>, <a>AUTHOR_2</a>')
+              .replaceAll('${authors-unlinked}', 'AUTHOR_1, AUTHOR_2')
+              .replaceAll('${author-unlinked}', 'AUTHOR_1, AUTHOR_2'));
 
       preview.textContent = previewHtml;
       hljs.highlightElement(preview);
 
       if (!isValidAo3ValidHtml(event.target.value)) {
         templateTextField.helperTextContent =
-          "This template appears to contain HTML tags that cannot be used on " +
-          "AO3, they have been removed from the preview";
+            'This template appears to contain HTML tags that cannot be used on ' +
+            'AO3, they have been removed from the preview';
         templateTextField.valid = false;
       } else {
-        templateTextField.helperTextContent = "";
+        templateTextField.helperTextContent = '';
         templateTextField.valid = true;
       }
     });
@@ -229,58 +213,55 @@ import { setCheckboxState, setInputValue, setupStorage } from "./utils.js";
 
   function isValidAo3ValidHtml(/** @type{string} */ html) {
     const sanitized = HtmlSanitizer.SanitizeHtml(html.trim());
-    const userDocument = DOM_PARSER.parseFromString(html.trim(), "text/html");
-    const sanitizedDocument = DOM_PARSER.parseFromString(
-      sanitized,
-      "text/html"
-    );
+    const userDocument = DOM_PARSER.parseFromString(html.trim(), 'text/html');
+    const sanitizedDocument =
+        DOM_PARSER.parseFromString(sanitized, 'text/html');
     return (
-      userDocument.documentElement.innerHTML ===
-      sanitizedDocument.documentElement.innerHTML
-    );
+        userDocument.documentElement.innerHTML ===
+        sanitizedDocument.documentElement.innerHTML);
   }
 
   // Import default body text from storage.
   (async () => {
-    const { title_template, summary_template, notes_template, workbody } =
-      await browser.storage.sync.get([
-        "title_template",
-        "summary_template",
-        "notes_template",
-        "workbody",
-      ]);
-    setInputValue(titleTemplate, title_template["default"]);
-    setInputValue(defaultBody, workbody["default"]);
-    setInputValue(summaryTemplate, summary_template["default"]);
-    setInputValue(notesTemplate, notes_template["default"]);
-    setCheckboxState(beginningNotesCheckbox, notes_template["begin"]);
-    setCheckboxState(endNotesCheckbox, notes_template["end"]);
+    const {title_template, summary_template, notes_template, workbody} =
+        await browser.storage.sync.get([
+          'title_template',
+          'summary_template',
+          'notes_template',
+          'workbody',
+        ]);
+    setInputValue(titleTemplate, title_template['default']);
+    setInputValue(defaultBody, workbody['default']);
+    setInputValue(summaryTemplate, summary_template['default']);
+    setInputValue(notesTemplate, notes_template['default']);
+    setCheckboxState(beginningNotesCheckbox, notes_template['begin']);
+    setCheckboxState(endNotesCheckbox, notes_template['end']);
   })();
 
   // When the form is submitted, save the default body text (without overriding
   // other options).
-  workForm.addEventListener("submit", async (submitEvent) => {
+  workForm.addEventListener('submit', async (submitEvent) => {
     submitEvent.preventDefault();
     await browser.storage.sync.set({
-      workbody: { default: defaultBody.value },
+      workbody: {default: defaultBody.value},
     });
     snackbar.open();
   });
-  titleForm.addEventListener("submit", async (submitEvent) => {
+  titleForm.addEventListener('submit', async (submitEvent) => {
     submitEvent.preventDefault();
     await browser.storage.sync.set({
-      title_template: { default: titleTemplate.value },
+      title_template: {default: titleTemplate.value},
     });
     snackbar.open();
   });
-  summaryForm.addEventListener("submit", async (submitEvent) => {
+  summaryForm.addEventListener('submit', async (submitEvent) => {
     submitEvent.preventDefault();
     await browser.storage.sync.set({
-      summary_template: { default: summaryTemplate.value },
+      summary_template: {default: summaryTemplate.value},
     });
     snackbar.open();
   });
-  notesForm.addEventListener("submit", async (submitEvent) => {
+  notesForm.addEventListener('submit', async (submitEvent) => {
     submitEvent.preventDefault();
     await browser.storage.sync.set({
       notes_template: {
@@ -292,8 +273,8 @@ import { setCheckboxState, setInputValue, setupStorage } from "./utils.js";
     snackbar.open();
   });
 
-  document.querySelector(".version").textContent =
-    browser.runtime.getManifest().version;
+  document.querySelector('.version').textContent =
+      browser.runtime.getManifest().version;
 
   // Set focus for a11y.
   titleTemplate.focus();
