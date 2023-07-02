8bc2b346fb103e686dd54caaa9a32c3836591c4c
jermowery <jermowery@gmail.com>
2023-07-01 20:01:43 -0700

    Remove service worker since Firefox doesn't support it

--- src/service-worker.js
+++
@@ -1,9 +0,0 @@
-import {ANALYTICS} from './google-analytics.js';
-
-addEventListener('unhandledrejection', async event => {
-  ANALYTICS.fireErrorEvent(event.reason);
-});
-
-chrome.runtime.onInstalled.addListener(() => {
-  ANALYTICS.fireEvent('install');
-});
