73446649d151256b82985afe1d3dd7b86ee87bff
jermowery <jermowery@gmail.com>
2023-11-27 22:57:43 -0800

    Attempt to may playwright open the popup

--- e2e/popup.spec.ts
+++ e2e/popup.spec.ts
@@ -1,5 +1,6 @@
 import {test, expect} from './fixture';
-test('popup page', async ({page, extensionId}) => {
+
+test('options page', async ({page, extensionId}) => {
   await page.goto(`chrome-extension://${extensionId}/options.html`);
   await expect(page.locator('header.app-bar')).toContainText(
     'AO3 Podfic Posting Helper Extension Options'
